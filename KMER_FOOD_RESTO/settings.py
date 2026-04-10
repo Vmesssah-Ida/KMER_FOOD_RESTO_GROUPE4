787e11542005094ca43e1b3e65599055def82bbd
Vmessah Ida <ivmessah@gmail.com>
2026-04-10 22:49:23 +0100

    Ajout de la base de donnees

--- KMER_FOOD_RESTO/settings.py
+++ KMER_FOOD_RESTO/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'KMER_FOOD_RESTO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Base_Restaurant',       
+        'USER': 'root',               
+        'PASSWORD': 'mysql123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
